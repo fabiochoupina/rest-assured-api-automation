02adfd7b23c7574c016003090c94841b60d6ae5b
fabiochoupina <soaresfabiojr@gmail.com>
2021-06-11 14:30:46 +1200

    create new login failed test

--- app/src/test/kotlin/rest/assured/api/automation/tests/RegisterTests.kt
+++ app/src/test/kotlin/rest/assured/api/automation/tests/RegisterTests.kt
@@ -1,15 +1,26 @@
 package rest.assured.api.automation.tests
 
+import io.restassured.RestAssured
 import io.restassured.RestAssured.given
+import io.restassured.builder.ResponseSpecBuilder
 import org.apache.http.HttpStatus
 import kotlin.test.Test
 import org.hamcrest.CoreMatchers.`is`
 import rest.assured.api.automation.domain.User
+import kotlin.test.BeforeTest
 
 class RegisterTests: BaseTest() {
 
+    @BeforeTest
+    fun setupRegister() {
+        RestAssured.responseSpecification = ResponseSpecBuilder()
+                .expectStatusCode(HttpStatus.SC_BAD_REQUEST)
+                .build()
+    }
+
     companion object {
         private const val REGISTER_USER_ENDPOINT: String = "/register"
+        private const val LOGIN_USER_ENDPOINT: String = "/login"
     }
 
     @Test
@@ -18,9 +29,19 @@ class RegisterTests: BaseTest() {
         given().
             body(user).
         `when`().
-            post(REGISTER_USER_ENDPOINT).
+            post(LOGIN_USER_ENDPOINT).
         then().
-            statusCode(HttpStatus.SC_BAD_REQUEST).
             body("error", `is`("Missing password"))
+    }
+
+    @Test
+    fun testLoginFailed() {
+        val user = User( email = "sydney@fife")
+            given().
+                body(user).
+            `when`().
+                post(REGISTER_USER_ENDPOINT).
+            then().
+                body("error", `is`("Missing password"))
     }
 }
